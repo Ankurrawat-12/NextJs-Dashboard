ecaa9842c9c241a279828db22cd7d5f44ea7b587
Ankur Rawat <ankurrawat620@gmail.com>
2024-01-25 03:47:02 +0530

    Completed The Course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import '@/app/ui/global.css';
-import {inter} from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard | @ankurrawat-12',
+    default: 'Acme Dashboard by @ankurrawat-12',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-acme.vercel.app'),
+  authors: [{name: 'Ankur Rawat', url:'https://www.linkedin.com/in/ankurrawat12'}],
+  keywords: ['Next.js 14', 'Acme', 'Dashboard', 'nextjs.org/learn', 'Server Actions'],
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    url: 'https://nextjs-dashboard-acme.vercel.app',
+    type: 'website',
+  },
+  twitter: {
+    site: '@acme',
+    description:'The official Next.js Learn Dashboard built with App Router.',
+    title:'Acme Dashboard by @ankurrawat-12',
+    creator:'@ankurrawat-12',
+  },
+};
 
 export default function RootLayout({
   children,
